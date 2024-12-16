8b7379d19c6b29241ccbd962b2efab7493d720e2
Luis Jorge <luis.jorge@guiafacil.com>
2024-12-16 14:25:01 -0300

    Adicionado snk

--- Unione.Net/Models/Configuration.cs
+++ Unione.Net/Models/Configuration.cs
@@ -2,10 +2,10 @@ namespace Unione.Net.Models;
 
 public class Configuration
 {
-    public string ServerAddress { get; set; }
-    public string ApiUrl { get; set; }
-    public string ApiVersion { get; set; }
-    public string ApiKey { get; set; }
-    public int ServerTimeout { get; set; }
+    public string ServerAddress { get; set; } = string.Empty;
+    public string ApiUrl => "en/transactional/api/";
+    public string ApiVersion => "v1";
+    public string ApiKey { get; set; } = string.Empty;
+    public int ServerTimeout { get; set; } = 30;
     public bool EnableLogging { get; set; }
 }
